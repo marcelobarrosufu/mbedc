fa3a367067bf214702e8d24ef5cb16ed56e42b9a
Marcelo Barros de Almeida <marcelobarrosalmeida@gmail.com>
2022-09-25 12:04:37 -0300

    more slides...

--- presentation/code/stack.c
+++ presentation/code/stack.c
@@ -1,16 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
 
-uint32_t global_sum = 0;
+uint32_t global_sum = 10;
 
-uint32_t sum(uint32_t *values, 
+uint32_t sum(uint32_t *values,
 			 uint32_t size)
 {
 	uint32_t sum = 0;
-	
+
 	for(size_t n = 0; n < size; n++)
 		sum += values[n];
-	
+
 	return sum;
 }
 
